b153b0edb5cf6dda6bde6a162258d3a14e28c68a
thaddavis <thaddadavis@gmail.com>
2016-03-21 14:27:06 -0400

    mailing list

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,11 +3,6 @@ class UserMailer < ApplicationMailer
 
   def contact_email(contact)
      @contact = contact
-
-     Rails.logger.debug "@@@@@@@@@@@@@@@@@@@@@@@@"
-     Rails.logger.debug "#{Rails.application.secrets.owner_email}"
-     Rails.logger.debug "@@@@@@@@@@@@@@@@@@@@@@@@"
-
      mail(to: Rails.application.secrets.owner_email, from: @contact.email, :subject => "Website")
   end
 end
